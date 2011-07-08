29e0e5a736e11baec6e4f36fc7e46d0e8662b9da
Guy Rozendorn <guy@rzn.co.il>
2011-07-09 02:49:38 +0300

    more tests

---
+++ src/infi/mpath/client/__init__.py
@@ -0,0 +1,127 @@
+
+import logging
+from infi.exceptools import chain
+from os.path import exists, sep, join
+
+DEFAULT_SOCKET = join(sep, 'var', 'run', 'multipathd.sock')
+DEFAULT_TIMEOUT = 3
+MAX_SIZE = 2 ** 8
+
+class ClientBaseException(Exception):
+    pass
+
+class ConnectionError(ClientBaseException):
+    pass
+
+class TimeoutExpired(ConnectionError):
+    pass
+
+class BaseConnection(object):
+    def __init__(self):
+        super(BaseConnection, self).__init__() #pragma: no cover
+
+    def connect(self):
+        raise NotImplementedError #pragma: no cover
+
+    def send(self, message):
+        raise NotImplementedError #pragma: no cover
+
+    def receive(self, message):
+        raise NotImplementedError #pragma: no cover
+
+    def disconnect(self):
+        raise NotImplementedError #pragma: no cover
+
+class UnixDomainSocket(BaseConnection):
+    def __init__(self, timeout=DEFAULT_TIMEOUT, address=DEFAULT_SOCKET):
+        super(UnixDomainSocket, self).__init__() #pragma: no cover
+        self._timeout = timeout
+        self._address = address
+        self._socket = None
+
+    def connect(self, address=None):
+        import socket
+        try:
+            socket_object = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+            socket_object.connect(self._address)
+            socket_object.settimeout(self._timeout)
+        except:
+            raise chain(ConnectionError)
+        self._socket = socket_object
+
+    def send(self, message):
+        from socket import timeout, error
+        logging.debug("sending %s", repr(message))
+
+        try:
+            bytes_sent = self._socket.send(message)
+        except timeout:
+            raise chain(TimeoutExpired)
+        except error:
+            raise chain(ConnectionError)
+
+        if bytes_sent < len(message):
+                self.send(message[bytes_sent:])
+
+    def receive(self, expected_length=MAX_SIZE):
+        from socket import timeout, error
+        if expected_length > MAX_SIZE:
+            return self.receive(MAX_SIZE) + self.receive(expected_length - MAX_SIZE)
+        try:
+            received_string = self._socket.recv(expected_length)
+            if len(received_string) < expected_length:
+                received_string += self.receive(expected_length - len(received_string))
+            return received_string
+        except timeout:
+            raise chain(TimeoutExpired)
+        except error:
+            raise chain(ConnectionError)
+
+    def disconnect(self):
+        self._socket.close()
+
+from contextlib import contextmanager, nested
+from logging import debug, exception
+
+from infi.instruct import Struct
+from infi.instruct import ULInt32, ULInt64
+from ctypes import c_size_t, sizeof
+
+HEADER_SIZE = sizeof(c_size_t)
+
+class MessageLength(Struct):
+            _fields_ = [(ULInt64 if HEADER_SIZE == 8 else ULInt32)("length"), ]
+
+class MultipathClient(object):
+    def __init__(self, connection=UnixDomainSocket()):
+        super(MultipathClient, self).__init__() #pragma: no cover
+        self._connection = connection
+
+    @contextmanager
+    def _with_connection_open(self):
+        try:
+            self._connection.connect()
+            yield
+        finally:
+            self._connection.disconnect()
+
+    def _get_message_size_as_string(self, message):
+        instance = MessageLength.create()
+        instance.length = len(message)
+        return MessageLength.instance_to_string(instance)
+
+    def _get_expected_message_size_from_string(self, string):
+        return MessageLength.create_instance_from_string(string).length
+
+    def _send_and_receive(self, message):
+        with self._with_connection_open():
+            self._connection.send(self._get_message_size_as_string(message))
+            self._connection.send(message)
+            stream = self._connection.receive(MessageLength.sizeof())
+            expected_length = self._get_expected_message_size_from_string(stream)
+            return self._connection.receive(expected_length).strip('\x00\n')
+
+    def rescan(self):
+        result = self._send_and_receive("reconfigure")
+        if result != 'ok':
+            raise RuntimeError(result) # TODO test this
---
+++ src/infi/mpath/client/tests.py
@@ -0,0 +1,198 @@
+
+import unittest
+import mock
+from logging import debug
+from contextlib import contextmanager
+
+from ctypes import sizeof, c_size_t
+
+TEST_MESSAGE_TO_SEND = 'reconfigure'
+TEST_MESSAGE_SIZE_TO_SEND = '\x0b' + '\x00' * (sizeof(c_size_t) - 1)
+TEST_MESSAGE_SIZE_TO_RECEIVE = '\x04' + '\x00' * (sizeof(c_size_t) - 1)
+TEST_MESSAGE_TO_RECEIVE = 'ok\n\x00'
+
+class ConnectionTestCase(unittest.TestCase):
+    def _get_connection_object(self):
+        raise unittest.SkipTest
+
+    def setUp(self):
+        self.connection = self._get_connection_object()
+
+    def test_connect(self):
+        self.connection.connect()
+
+    def test_connect_and_disconnect(self):
+        self.connection.connect()
+        self.connection.disconnect()
+
+    def test_send_and_receive(self):
+        self.connection.connect()
+        self.connection.send(TEST_MESSAGE_SIZE_TO_SEND)
+        self.connection.send(TEST_MESSAGE_TO_SEND)
+        self.assertEquals(self.connection.receive(len(TEST_MESSAGE_SIZE_TO_RECEIVE)),
+                          TEST_MESSAGE_SIZE_TO_RECEIVE)
+        self.assertEquals(self.connection.receive(len(TEST_MESSAGE_TO_RECEIVE)),
+                          TEST_MESSAGE_TO_RECEIVE)
+        self.connection.disconnect()
+
+class UnixDomainSocketTestCase(ConnectionTestCase):
+    def _get_connection_object(self):
+        from . import  UnixDomainSocket
+        return UnixDomainSocket()
+
+    def setUp(self):
+        from platform import system
+        if system().lower() != 'linux':
+            raise unittest.SkipTest
+        super(UnixDomainSocketTestCase, self).setUp()
+
+def send_mock(*args, **kwargs):
+    message = kwargs.get('string', args[0])
+    return len(message)
+
+def recv_mock(items_to_return):
+    items = [item for item in items_to_return]
+    def side_effect(*args, **kwargs):
+        return items.pop()
+    return side_effect
+
+class MockUnixDomainSocketTestCase(UnixDomainSocketTestCase):
+    def setUp(self):
+        with mock.patch("platform.system") as system:
+            system.return_value = 'Linux'
+            super(UnixDomainSocketTestCase, self).setUp()
+
+    @contextmanager
+    def mock_socket(self):
+        with mock.patch("socket.socket") as socket:
+            socket.return_value = mock.Mock()
+            socket.return_value.send.side_effect = send_mock
+            yield socket
+
+    def test_connect(self):
+        from socket import AF_UNIX, SOCK_STREAM
+        with self.mock_socket() as socket:
+            super(MockUnixDomainSocketTestCase, self).test_connect()
+        self.assertTrue(socket.called)
+        self.assertEquals(socket.call_count, 1)
+        self.assertEquals(socket.call_args[0], (AF_UNIX, SOCK_STREAM))
+
+    def test_connect_and_disconnect(self):
+        from socket import AF_UNIX, SOCK_STREAM
+        with self.mock_socket() as socket:
+            super(MockUnixDomainSocketTestCase, self).test_connect_and_disconnect()
+        self.assertTrue(socket.called)
+        self.assertEquals(socket.call_count, 1)
+        self.assertEquals(socket.call_args[0], (AF_UNIX, SOCK_STREAM))
+        self.assertEquals(socket.return_value.close.call_count, 1)
+
+    def test_send_and_receive(self):
+        with self.mock_socket() as socket:
+            _recv_mock = recv_mock([TEST_MESSAGE_TO_RECEIVE, TEST_MESSAGE_SIZE_TO_RECEIVE])
+            socket.return_value.recv.side_effect = _recv_mock
+            super(MockUnixDomainSocketTestCase, self).test_send_and_receive()
+        self.assertEquals(socket.return_value.send.call_count, 2)
+
+    def _test_send_and_receive_long_messages(self):
+        from . import MessageLength
+        message_to_send = 'abcd' * 2048
+        self.connection.connect()
+        instance = MessageLength.create()
+        instance.length = len(message_to_send)
+        self.connection.send(MessageLength.instance_to_string(instance))
+        self.connection.send(message_to_send)
+        self.assertEquals(self.connection.receive(2048), 'a' * 2048)
+        self.connection.disconnect()
+
+    def test_send_and_receive_long_messages(self):
+        def send_mock(*args, **kwargs):
+            message = kwargs.get('string', args[0])
+            return 1 if len(message) == 1 else len(message) / 2
+
+        def recv_mock(*args, **kwargs):
+            expected_size = kwargs.get('bufsize', args[0])
+            return 'a' * (1 if expected_size == 1 else expected_size / 2)
+
+        with self.mock_socket() as socket:
+            socket.return_value.send.side_effect = send_mock
+            socket.return_value.recv.side_effect = recv_mock
+            self._test_send_and_receive_long_messages()
+        self.assertEquals(socket.return_value.send.call_count, 18)
+        self.assertEquals(socket.return_value.recv.call_count, 72)
+
+    def test_connect__init_raises_exception(self):
+        from socket import error
+        from . import ConnectionError
+        with self.mock_socket() as socket:
+            socket.side_effect = error
+            self.assertRaises(ConnectionError, self.connection.connect)
+
+    def test_connect__connect_raises_exception(self):
+        from socket import error
+        from . import ConnectionError
+        with self.mock_socket() as socket:
+            socket.return_value.connect.side_effect = error
+            self.assertRaises(ConnectionError, self.connection.connect)
+
+    def test_connect__settimeout_raises_exception(self):
+        from socket import error
+        from . import ConnectionError
+        with self.mock_socket() as socket:
+            socket.return_value.settimeout.side_effect = error
+            self.assertRaises(ConnectionError, self.connection.connect)
+
+    def test_send__raises_error(self):
+        from socket import error
+        from . import ConnectionError
+        with self.mock_socket() as socket:
+            socket.return_value.send.side_effect = error
+            self.connection.connect()
+            self.assertRaises(ConnectionError, self.connection.send, *['foo'])
+
+    def test_send__raises_timeout(self):
+        from socket import timeout
+        from . import TimeoutExpired
+        with self.mock_socket() as socket:
+            socket.return_value.send.side_effect = timeout
+            self.connection.connect()
+            self.assertRaises(TimeoutExpired, self.connection.send, *['foo'])
+
+    def test_recv__raises_error(self):
+        from socket import error
+        from . import ConnectionError
+        with self.mock_socket() as socket:
+            socket.return_value.recv.side_effect = error
+            self.connection.connect()
+            self.assertRaises(ConnectionError, self.connection.receive, *[1])
+
+    def test_recv__raises_timeout(self):
+        from socket import timeout
+        from . import TimeoutExpired
+        with self.mock_socket() as socket:
+            socket.return_value.recv.side_effect = timeout
+            self.connection.connect()
+            self.assertRaises(TimeoutExpired, self.connection.receive, *[1])
+
+class MultipathClientTestCase(unittest.TestCase):
+    def setUp(self):
+        from platform import system
+        from . import MultipathClient
+        if system().lower() != 'linux':
+            raise unittest.SkipTest
+        super(MultipathClientTestCase, self).setUp()
+        self.client = MultipathClient()
+
+    def test_rescan(self):
+        self.client.rescan()
+
+class MutipathClientSimulatorTestCase(MultipathClientTestCase):
+    def setUp(self):
+        from ..simulator import SimulatorConnection, Singleton
+        from . import MultipathClient
+        super(MultipathClientTestCase, self).setUp()
+        self.client = MultipathClient(SimulatorConnection())
+        self.simulator = Singleton()
+
+    def test_rescan(self):
+        MultipathClientTestCase.test_rescan(self)
+        self.assertIn('reconfigure', self.simulator._handled_messages)
