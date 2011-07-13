ac0b1872905735f34d783880be13f0283c0d758a
Guy Rozendorn <guy@rzn.co.il>
2011-07-13 15:12:50 +0300

    module rename

--- src/infi/multipathtools/dtypes/__init__.py
+++ src/infi/multipathtools/dtypes/__init__.py
@@ -1,19 +1,20 @@
 
 from bunch import Bunch
+
 class MultipathDevice(Bunch):
     def __init__(self, id, device_name, minor):
         super(MultipathDevice, self).__init__()
         self.path_groups = []
         self.id = id
         self.device_name = device_name
-        self.major_minor = '253:%s' % minor.replace('dm-', '')
+        self.major_minor = ('253:%s' % minor.replace('dm-', '')).split(":")
 
 class PathGroup(Bunch):
     def __init__(self, state, priority):
         super(PathGroup, self).__init__()
         self.paths = []
         self.state = state # enabled, disabled, active, undef
-        self.priority = priority
+        self.priority = int(priority)
         self.load_balancing_policy = 'round-robin'
 
 class Path(Bunch):
@@ -21,7 +22,10 @@ class Path(Bunch):
         super(Path, self).__init__()
         self.id = id
         self.device_name = device_name
-        self.major_minor = major_minor
+        self.major_minor = major_minor.split(":")
         self.state = state # active, failed, or undef
-        self.priority = priority
-        self.hctl = hctl
+        self.priority = int(priority)
+        self.hctl = hctl.split(":")
+
+PATHGROUP_STATES = ['enabled', 'disabled', 'active', 'undef']
+PATH_STATES = ['active', 'failed', 'undef']
